97ea0d5620163a3af4ac4b490a7f746a578b4eef
Omar Hernández Rey <hernandezreyomar@gmail.com>
2025-07-29 11:20:19 -0500

    6 Tu primer test

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,16 @@
+const {sum, multiply, divide} = require('./02-math');
+
+test('adds 1 + 3 to equal 4', () => {
+  const rta = sum(1, 3);
+  expect(rta).toBe(4);
+});
+
+test('multiplies 2 * 3 to equal 6', () => {
+  const rta = multiply(2, 3);
+  expect(rta).toBe(6);
+});
+
+test('divides 6 / 2 to equal 3', () => {
+  const rta = divide(6, 2);
+  expect(rta).toBe(3);
+});
