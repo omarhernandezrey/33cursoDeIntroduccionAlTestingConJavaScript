31fa0ff6f73ac48c88be0d8e2c832737d94896e1
Omar Hernández Rey <hernandezreyomar@gmail.com>
2025-08-04 11:18:23 -0500

    16 Proyecto de API

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,14 +2,20 @@ const Person = require('./06-person');
 
 describe('Test for Person', () => {
   let person;
-
+  // Arrange
   beforeEach(() => {
     person = new Person('Omar', 45, 1.7);
   });
 
   test('should return down', () => {
+    // AAA
+    // Arrange // Given
+    // Act // When
+    // Assert // Then
     person.weight = 45;
+    // Act
     const imc = person.calcIMC();
+    // Assert
     expect(imc).toBe('down');
   });
 
