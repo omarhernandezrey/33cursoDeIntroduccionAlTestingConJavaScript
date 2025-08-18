2f064c07c45fb82dbe837f5ec2124d61cfd4a8df
omarhernandezrey <hernandezreyomar@gmail.com>
2025-08-18 14:52:05 -0500

    23 e2e

---
+++ examples/api/api/e2e/books.integrations.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+
+const createApp = require('../src/app');
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test for [GET] /api/v1/books', () => {
+    test('should return a list books', () => {
+      // Arrange
+      console.log('');
+      // Act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(1);
+        });
+    });
+  });
+});
